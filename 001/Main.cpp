aa8f4d04c4c47e2c6987f28c4ba15b6127ccf25f
Warapob Keatkongsang <warapob2544@gmail.com>
2020-09-03 22:02:50 +0700

    movement

--- 001/Main.cpp
+++ 001/Main.cpp
@@ -3,17 +3,35 @@
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(800, 600), "Game01");
-    sf::Texture player;
-    sf::Sprite shapeSprite;
-    shapeSprite.setTexture(player);
-    int spriteSizeX = player.getSize().x/3;
-    int spriteSizeY = player.getSize().y/4;
-    shapeSprite.setTextureRect(sf::IntRect(0, 0, spriteSizeX, spriteSizeY));
-    while (window.isOpen())                                                         //gameloop
+    sf::RectangleShape player(sf::Vector2f(100.0f, 100.0f));
+    while (window.isOpen())
     {
-        window.draw(shapeSprite);
-        window.display();
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {
+            player.move(-0.5f,0.0f);
+            printf("Keypress : A\n");
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) {
+            player.move(0.5f, 0.0f);
+            printf("Keypress : D\n");
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {
+            player.move(0.0f, -0.5f);
+            printf("Keypress : W\n");
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) {
+            player.move(0.0f, 0.5f);
+            printf("Keypress : S\n");
+        }
         window.clear();
+        window.draw(player);
+        window.display();
+        
     }
 
     return 0;
