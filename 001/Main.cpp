9a8c308776ed4009eb82f2429ebd033b1d939701
Warapob Keatkongsang <warapob2544@gmail.com>
2020-09-04 12:08:43 +0700

    function keyboardInput

--- 001/Main.cpp
+++ 001/Main.cpp
@@ -1,33 +1,39 @@
 #include <SFML/Graphics.hpp>
+sf::RectangleShape player(sf::Vector2f(100.0f, 100.0f));
+void keyboardInput() {
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {
+        player.move(-0.5f, 0.0f);
+        printf("Keypress : A\n");
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) {
+        player.move(0.5f, 0.0f);
+        printf("Keypress : D\n");
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {
+        player.move(0.0f, -0.5f);
+        printf("Keypress : W\n");
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) {
+        player.move(0.0f, 0.5f);
+        printf("Keypress : S\n");
+    }
+}
 
 int main()
 {
+    
     sf::RenderWindow window(sf::VideoMode(800, 600), "Game01");
-    sf::RectangleShape player(sf::Vector2f(100.0f, 100.0f));
+    
     while (window.isOpen())
     {
+        bool w = true, a = true, s = true, d = true;
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
                 window.close();
             }
         }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {
-            player.move(-0.5f,0.0f);
-            printf("Keypress : A\n");
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) {
-            player.move(0.5f, 0.0f);
-            printf("Keypress : D\n");
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {
-            player.move(0.0f, -0.5f);
-            printf("Keypress : W\n");
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) {
-            player.move(0.0f, 0.5f);
-            printf("Keypress : S\n");
-        }
+        keyboardInput();
         window.clear();
         window.draw(player);
         window.display();
